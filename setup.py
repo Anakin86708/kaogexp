aa157aa5f8cc9f28b12221fa74190032039f0eb7
Ariel Tadeu da Silva <silva.ariel2013@gmail.com>
2022-02-01 14:59:54 -0300

    Update setup.py.
    Signed-off-by: Ariel Tadeu da Silva <silva.ariel2013@gmail.com>

--- setup.py
+++ setup.py
@@ -1,26 +1,14 @@
 from setuptools import setup
 
+with open("requirements.txt", "r") as fh:
+    requirements = fh.readlines()
+
 setup(
     name='kaogexp',
-    version='0.1',
+    version='0.2',
     packages=['kaogexp'],
-    package_dir={'': 'test'},
-    url='',
-    license='',
     author='Ariel Tadeu da Silva',
-    author_email='silva.ariel2013@gmail.com',
+    author_email='silva.ariel@icloud.com',
     description='Created to explain instances from ML models.',
-    install_requires=[
-        'pandas',
-        'numpy',
-        'distython',
-        'sklearn',
-        'torch',
-        'kaog',
-        'lhsmdu',
-        'requests',
-        'seaborn',
-        'matplotlib',
-        'setuptools'
-    ],
+    install_requires=[req.strip() for req in requirements if req[:2] != "# "],
 )
