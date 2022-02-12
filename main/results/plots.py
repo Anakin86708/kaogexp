6d44e02d0a18eec4809facd519873a9c491c588d
Ariel Tadeu da Silva <silva.ariel2013@gmail.com>
2022-02-12 11:52:14 -0300

    Normalização nos plots do CARLA, utilizando valores das dimensões, segundo a quantidade de features aceitas no modelo.
    Signed-off-by: Ariel Tadeu da Silva <silva.ariel2013@gmail.com>

--- main/results/plots.py
+++ main/results/plots.py
@@ -8,6 +8,8 @@ from matplotlib import pyplot as plt
 working_dir = os.path.dirname(__file__)
 dir_carla_results = 'E:\\Users\\silva\\PycharmProjects\\CARLA\\experiments\\results'
 dir_kaogexp_results = os.path.join(working_dir, '..', 'carla_runs')
+dir_dropped = os.path.join(working_dir, 'dropped')
+os.makedirs(dir_dropped, exist_ok=True)
 results_datasets_dirs = ['adult', 'compas', 'credit']
 
 cols_dist = ['Distance_1', 'Distance_2', 'Distance_3', 'Distance_4']
@@ -18,16 +20,16 @@ data = {}
 # Extracted from CARLA paper
 dimensions = {
     'adult': 20,
-    'compas': 8,
-    'give-me-some-credit': 11
+    'compas': 10,
+    'give-me-some-credit': 10
 }
 
 
 def treat_data(df, dataset):
     df = df.applymap(lambda x: x / dimensions[dataset])
-    df_ = df.where(df <= 1).dropna()
-    assert df_.max().max() <= 1 and df_.min().min() >= 0
-    return df_
+    df_dropped = df.T.where(df.T > 1).dropna().T
+    df_ = df.T.where(df.T <= 1).dropna().T
+    return df_, df_dropped
 
 
 kaogexp_dists = {}
@@ -41,9 +43,15 @@ for filename in files:
     file_result_kaogexp = os.path.join(dir_kaogexp_results, dataset_, f'{dataset_}.result')
     with open(file_result_kaogexp, 'r') as file:
         distancias_kaogexp = pd.DataFrame(eval('[' + file.readlines()[16:][0].replace('}', '}, ') + ']'))
-        treat_data1 = treat_data(distancias_kaogexp, dataset)
+        treat_data1, dropped_kaogexp = treat_data(distancias_kaogexp, dataset)
         treat_data1.columns = cols_dist
         kaogexp_dists[dataset] = treat_data1
+
+        # Save dropped, if exits
+        if not dropped_kaogexp.empty:
+            method_ = 'kaogexp'
+            dataset_ = dataset.replace('_', '-')
+            dropped_kaogexp.to_csv(os.path.join(dir_dropped, f'{method_}_{dataset_}_dropped.csv'))
 
     df = pd.read_csv(os.path.join(dir_carla_results, filename))
     try:
@@ -60,13 +68,18 @@ for distance in cols_dist:
         df = df.append(distance_)
         for method, df_ in methods.items():
             try:
-                df_cols = treat_data(pd.DataFrame([df_[distance]]), dataset).iloc[0]
+                df_cols, dropped_df = treat_data(pd.DataFrame([df_[distance]]), dataset)
+                df_cols = df_cols.iloc[0]
+
+                if not dropped_df.empty:
+                    method_ = method.replace('_', '-')
+                    dataset_ = dataset.replace('_', '-')
+                    dropped_df.to_csv(os.path.join(dir_dropped, f'{method_}_{dataset_}_dropped.csv'))
+
                 df_cols.name = method
                 df = df.append(df_cols)
             except (ValueError, TypeError):
                 print(f'Error in {method} - {dataset}')
-            except AssertionError:
-                print(f'AssertionError in {method} - {dataset}')
 
         plt.clf()
         fig = plt.figure()
