b33c86c31b118fa032047d6760778bee38135145
Ariel Tadeu da Silva <silva.ariel2013@gmail.com>
2022-02-14 07:52:55 -0300

    Resultados compas sem otimização.
    Signed-off-by: Ariel Tadeu da Silva <silva.ariel2013@gmail.com>

--- main/carla_runs/compas/compas_multithread.py
+++ main/carla_runs/compas/compas_multithread.py
@@ -68,7 +68,7 @@ threads_num = multiprocessing.cpu_count()
 
 def explicar(item, i, total):
     logging.info('\n' + ('#' * 15) + f' Item {i + 1} of {total} ' + ('#' * 15) + '\n')
-    explicador = KAOGExp(train_data, model, sampler, fixed_cols=fixed_cols, otimizar=True)
+    explicador = KAOGExp(train_data, model, sampler, fixed_cols=fixed_cols, otimizar=False)
     return explicador.explicar(item, metodo=metodo, classe_desejada=classe_desejada,
                                tratador_associado=tratador_associado, normalizador_associado=normalizador_associado)
 
@@ -118,7 +118,8 @@ cerscore = cers.calcular(explicacoes, proximidades)
 cerscore_carla = {}
 for distance in carla_distances[0].keys():
     op = operator.itemgetter(distance)
-    cerscore_carla[distance] = cers.calcular(explicacoes, list(map(op, carla_distances)))
+    cerscore_carla[distance] = cers.calcular(explicacoes,
+                                             list(map(op, list(filter(lambda x: x is not None, carla_distances)))))
 
 metricas_dict = {
     'validade': {
